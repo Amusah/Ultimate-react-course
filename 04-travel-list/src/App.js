063a036428775385d17e526cfeafa7f525a5af43
Amusah <henryamusah@gmail.com>
2024-02-19 19:23:17 +0000

    Building a form and handling submissions

--- 04-travel-list/src/App.js
+++ 04-travel-list/src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Space suit", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 2, description: "Charger", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -22,10 +22,30 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("Hi");
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {/* {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))} */}
+        {[...Array(20).keys()]
+          .map((n) => n + 1)
+          .map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -34,7 +54,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
