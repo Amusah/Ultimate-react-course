0882486a9b3af0dca7a9da18c7d93b2d354a30cc
Amusah <henryamusah@gmail.com>
2024-02-24 23:48:05 +0000

    Lifting state

--- 04-travel-list/src/App.js
+++ 04-travel-list/src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,18 +26,20 @@ function Logo() {
   return <h1>🚀 Far Away 🪐</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    if(!description) return;
+    if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
 
-    setDescription('');
+    onAddItems(newItem);
+
+    setDescription("");
     setQuantity(1);
   }
   return (
@@ -66,11 +73,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
