2793bec456fd40294b55e7534536c1801aa38a55
Amusah <henryamusah@gmail.com>
2024-03-27 13:20:29 +0000

    Type checking with PropTypes

--- 06-usepopcorn/src/StarRating.js
+++ 06-usepopcorn/src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,7 +11,15 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const StarRating = ({ maxRating = 5, color = "#fcc419", size = 48, className="", messages = [], defaultRating = 0, onSetRating }) => {
+const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
   const [rating, setRating] = useState(defaultRating);
   const [tempRating, SetTempRating] = useState(0);
 
@@ -23,7 +32,7 @@ const StarRating = ({ maxRating = 5, color = "#fcc419", size = 48, className="",
     lineHeight: "1",
     margin: "0",
     color,
-    fontSize: `${size / 1.5}px`
+    fontSize: `${size / 1.5}px`,
   };
 
   return (
@@ -49,17 +58,29 @@ const StarRating = ({ maxRating = 5, color = "#fcc419", size = 48, className="",
           />
         ))}
       </div>
-      <p style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 };
 
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func
+};
 
 const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
-    height: "48px",
     display: "block",
     cursor: "pointer",
   };
