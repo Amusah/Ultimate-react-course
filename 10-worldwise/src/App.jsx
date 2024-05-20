4a8228ca7e4754c0e8ae422f2b329a3f92eeb5f7
Amusah <henryamusah@gmail.com>
2024-05-20 12:01:59 +0000

    Using css modules and global css

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -3,16 +3,17 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
 
 function App() {
   return (
     <div>
-      <h1>This is a static text</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
--- 10-worldwise/src/components/PageNav.jsx
+++ 10-worldwise/src/components/PageNav.jsx
@@ -1,17 +1,24 @@
 import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink className={styles.li} to="/">
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink className={styles.li} to="/pricing">
+            Pricing
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink className={styles.li} to="/product">
+            Product
+          </NavLink>
         </li>
       </ul>
     </nav>
