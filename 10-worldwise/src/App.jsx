eeb07d73c574d0a1cb068f2d57e0ffd388524ba3
Amusah <henryamusah@gmail.com>
2024-05-20 14:29:55 +0000

    Done building and routing  pages

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 
@@ -13,8 +14,9 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
-          <Route path='*' element={<PageNotFound />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
--- 10-worldwise/src/components/PageNav.jsx
+++ 10-worldwise/src/components/PageNav.jsx
@@ -1,24 +1,20 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from './Logo'
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
-          <NavLink className={styles.li} to="/">
-            Home
-          </NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink className={styles.li} to="/pricing">
-            Pricing
-          </NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink className={styles.li} to="/product">
-            Product
-          </NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>Login</NavLink>
         </li>
       </ul>
     </nav>
