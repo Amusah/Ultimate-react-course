823290baeb3703e600169f2628096151186dc7a3
Amusah <henryamusah@gmail.com>
2024-05-21 10:31:59 +0000

    nested routes and index routes

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -11,11 +11,19 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route index element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+
+          {/* nested routes */}
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>List of cities</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
+
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
