624fff591af813a36a0dc1a4a3ce88d3292b0866
Amusah <henryamusah@gmail.com>
2024-06-17 11:21:10 +0000

    implemented login route

--- 10-worldwise/src/App.jsx
+++ 10-worldwise/src/App.jsx
@@ -12,34 +12,37 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
 
-          {/* nested routes */}
-          <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              // redirect mechanism
-              element={<Navigate replace to="cities" />}
-            />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
+            {/* nested routes */}
+            <Route path="app" element={<AppLayout />}>
+              <Route
+                index
+                // redirect mechanism
+                element={<Navigate replace to="cities" />}
+              />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
 
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
