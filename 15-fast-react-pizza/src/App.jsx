90488f5cf1c73216989a9c34d5868133a6fd011c
Amusah <henryamusah@gmail.com>
2024-08-29 22:34:48 +0000

    Fetching Orders

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, {loader as OrderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from './ui/Error';
@@ -28,12 +28,14 @@ const router = createBrowserRouter([
         element: <Cart />,
       },
       {
-        path: "/order",
-        element: <Order />,
-      },
-      {
         path: "/order/new",
         element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: OrderLoader,
+        errorElement: <Error />,
       },
     ],
   },
