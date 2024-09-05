e6c7e9c49bc9c9fefb328d364b60d42ae8ef2d1e
Amusah <henryamusah@gmail.com>
2024-09-05 05:58:52 +0000

    Css grid

--- 15-fast-react-pizza/src/ui/AppLayout.jsx
+++ 15-fast-react-pizza/src/ui/AppLayout.jsx
@@ -9,15 +9,16 @@ function AppLayout() {
   // console.log(navigation);
 
   return (
-    <div className="layout">
+    <div className="h-screen grid grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
 
-      <main>
-      
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="max-w-3xl mx-auto">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverView />
     </div>
