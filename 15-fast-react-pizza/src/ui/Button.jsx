0f6d8054774373612c5c50a9038e8a3e40ccec0d
Amusah <henryamusah@gmail.com>
2024-09-12 01:41:26 +0000

    styling the cart page

--- 15-fast-react-pizza/src/ui/Button.jsx
+++ 15-fast-react-pizza/src/ui/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "px-4 py-2.5 md:px-6 md:py-3.5 border-2 hover:text-stone-800 hover:bg-stone-300 border-stone-300 uppercase font-semibold tracking-wide text-stone-400 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:bg-yellow-300 focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-4",
   };
 
   if (to)
