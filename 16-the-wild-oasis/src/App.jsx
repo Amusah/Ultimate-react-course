2fa6837c602916aa1441c9913690eb6749047a86
Amusah <henryamusah@gmail.com>
2024-11-30 02:11:14 +0000

    more reusable components

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orangered;
+  /* background-color: orangered; */
   padding: 20px;
 `;
 
@@ -14,8 +15,10 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Button>Check in</Button>
+        <Row type="horizontal">
+          <Heading as="h1">The Wild Oasis</Heading>
+          <Button variation="primary" size="medium">Check in</Button>
+        </Row>
         <Input type="number" placeholder="Number of guests" />
         <Heading as="h2">The Wild Oasis</Heading>
       </StyledApp>
